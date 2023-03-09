88132c670d154f6c075a4197d2e5f2b8be3b5b04
Marcelo Araya Briceño <98577347+TheMarchelo@users.noreply.github.com>
2023-03-08 18:34:56 -0600

    Creacion

---
+++ src/main/java/com/examen1/TheMarcheloMarceloArayaBriceOCasoPractico1Application.java
@@ -0,0 +1,13 @@
+package com.examen1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheMarcheloMarceloArayaBriceOCasoPractico1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheMarcheloMarceloArayaBriceOCasoPractico1Application.class, args);
+	}
+
+}
